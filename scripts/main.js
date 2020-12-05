037f883ea16ea6fd2a43b6616a57bb60528588c6
Eduardo <ex.romeroma@gmail.com>
2020-12-05 12:03:08 +0100

    Added logic for math basic operators, also display of values before and after an operator is pressed.

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,22 @@
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator__keyPressed")
 const display = document.querySelector(".calculator__display")
-
+//Calculate function, takes 3 args, performs operations
+const operate = (num1, operator, num2) => {
+    let result = '';
+    //Because values from num1 and num2 come from button press and are value-type strings
+    //convert strings to floats by parseFloat
+    if (operator == 'add') {
+        result = parseFloat(num1) + parseFloat(num2);
+    } else if (operator === 'subtract') {
+        result = parseFloat(num1) - parseFloat(num2);
+    } else if (operator === 'divide') {
+        result = parseFloat(num1) / parseFloat(num2);
+    } else if (operator === 'multiply') {
+        result = parseFloat(num1) * parseFloat(num2);
+    }
+    return result;
+}
 // Function to store the id of the button pressed:
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
@@ -13,42 +28,53 @@ keys.addEventListener('click', e => {
         // Storing the value of the displayed number on displayNum
         const keyContent = key.textContent;
         const displayNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType;
+
         // If button pressed is not an operator:
         if (!action) {
+
             // If calc display == 0, replace display text with pressed key 
-            if (displayNum === '0') {
-                display.textContent = keyContent;
+            // or previousKeyType is operator, replace display with new pressed number
+            if (displayNum === '0' || previousKeyType === 'key--operator') {
+                display.textContent = keyContent
+                calculator.dataset.previousKeyType = 'not--operator'
                 // Else, append != 0 number to the current number on display
             } else {
-                display.textContent = displayNum + keyContent;
+                display.textContent = displayNum + keyContent
             }
+            Array.from(document.getElementsByClassName('operator')).forEach(el => el.classList.remove('is-depressed'));
         }
+        // If button pressed is an operator:
         if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-            console.log('operator key pressed')
+            // To acknowledge the previous and next value
+            // adding the class is depressed so the operator is highlighted
+            key.classList.add('.is-depressed');
+            // Updating the display to the pressed key, first recognise 
+            // if previous key is an operator key.
+            // Adding custom attribute => data-previous-key-type
+            calculator.dataset.previousKeyType = 'key--operator';
+            calculator.dataset.firstValue = displayNum;
+            calculator.dataset.operator = action;
         }
         if (action === 'decimal') {
+            display.textContent = displayNum + '.'
             console.log('decimal key pressed')
         }
-        if (action === 'clear') {
+        if (action === 'clear-display') {
             console.log('clear key pressed')
         }
-        if (action === 'calculate') {
+        if (action === 'operate') {
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayNum
+
+            display.textContent = operate(firstValue, operator, secondValue)
             console.log('equal key pressed')
         }
+        // Removing the .is-depressed class from buttons so we can introduce
+        // the next number of the operation
     }
 })
 
-
-// Clicking on button displays that button's value on screen
-// Note: valueKeyPressed comes back as string, parseInt first to make any operations
-// function displayNumber(elem) {
-//     document.getElementById('display').value += elem;
-// }
-
-
-// // Clear screen of input, return display to 0. Bug: Currently showing two zeros instead of one
-// function clearScreen() {
-//     document.getElementById('display').value = document.getElementById('clear').value;
-// }
 
 
